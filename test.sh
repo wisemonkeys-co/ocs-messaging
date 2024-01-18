6c823624ca7e17eb321c5e88e63117e501ae59ca
Daniel Marcos Fragoso de Souza <daniel.souza@minutrade.com>
2024-01-17 23:02:58 -0300

    actions chore: test script

--- test.sh
+++
@@ -1,9 +0,0 @@
-CURRENT_TAG=$(git fetch origin | git tag -l | grep $TAG);
-echo 'asd '"$CURRENT_TAG"' asd '"$TAG";
-if [[ -z "$CURRENT_TAG" ]]; then
-  # git tag $TAG;
-  # git push origin $TAG;
-  echo 'Version '"$TAG"' published'
-else
-  echo "Nothing to publish";
-fi
