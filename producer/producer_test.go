600bb702b31aeca4125f75111766ae86e460c196
Daniel Marcos Fragoso de Souza <daniel.souza@minutrade.com>
2024-02-07 20:08:08 -0300

    asyncProducer feat: add producer async mode

--- producer/producer_test.go
+++ producer/producer_test.go
@@ -2,8 +2,10 @@ package producer
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -17,7 +19,9 @@ var logChannel chan types.LogEvent
 var topicName string
 var msv testutils.MockSchemaValidator
 var consumer *kafka.Consumer
-var producer KafkaProducer
+var syncProducer KafkaProducer
+var asyncProducer KafkaProducer
+var deliveryReportChan chan types.EventReport
 
 func TestSendMessage(t *testing.T) {
 	defer msv.FlushEncodeReturnList()
@@ -27,12 +31,12 @@ func TestSendMessage(t *testing.T) {
 	valueSchemaId := 6
 	msv.SetReturnedValue([]byte{0, 0, 0, 0, 5, 107, 101, 121}, nil)          // "key"
 	msv.SetReturnedValue([]byte{0, 0, 0, 0, 6, 118, 97, 108, 117, 101}, nil) // "value"
-	sendMessageError := producer.SendSchemaBasedMessage(topicName, key, value, keySchemaId, valueSchemaId)
+	sendMessageError := syncProducer.SendSchemaBasedMessage(topicName, key, value, keySchemaId, valueSchemaId)
 	if sendMessageError != nil {
 		t.Error(sendMessageError)
 		return
 	}
-	timeout, _ := time.ParseDuration("20s")
+	timeout, _ := time.ParseDuration("5s")
 	message, consumerError := consumer.ReadMessage(timeout)
 	if consumerError != nil {
 		t.Error(consumerError)
@@ -57,12 +61,12 @@ func TestSendMessageWithoutKey(t *testing.T) {
 	value := "value"
 	valueSchemaId := 6
 	msv.SetReturnedValue([]byte{0, 0, 0, 0, 6, 118, 97, 108, 117, 101}, nil) // "value"
-	sendMessageError := producer.SendSchemaBasedMessage(topicName, nil, []byte(value), 0, valueSchemaId)
+	sendMessageError := syncProducer.SendSchemaBasedMessage(topicName, nil, []byte(value), 0, valueSchemaId)
 	if sendMessageError != nil {
 		t.Error(sendMessageError)
 		return
 	}
-	timeout, _ := time.ParseDuration("20s")
+	timeout, _ := time.ParseDuration("5s")
 	message, consumerError := consumer.ReadMessage(timeout)
 	if consumerError != nil {
 		t.Error(consumerError)
@@ -80,6 +84,121 @@ func TestSendMessageWithoutKey(t *testing.T) {
 	consumedValue := string(message.Value[5:])
 	if consumedValue != value {
 		t.Errorf("Error on write data")
+		return
+	}
+}
+
+func TestSendMessageAsyncModeSuccess(t *testing.T) {
+	defer msv.FlushEncodeReturnList()
+	eventDataList := []struct {
+		key   string
+		value string
+	}{
+		{
+			key:   "key",
+			value: "value",
+		},
+		{
+			key:   "ye",
+			value: "val",
+		},
+		{
+			key:   "ky",
+			value: "lue",
+		},
+		{
+			key:   "ek",
+			value: "vue",
+		},
+	}
+	keySchemaId := 5
+	valueSchemaId := 6
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 5, 107, 101, 121}, nil)          // "key"
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 6, 118, 97, 108, 117, 101}, nil) // "value"
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 5, 121, 101}, nil)               // "ye"
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 6, 118, 97, 108}, nil)           // "val"
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 5, 107, 121}, nil)               // "ky"
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 6, 108, 117, 101}, nil)          // "lue"
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 5, 101, 107}, nil)               // "ek"
+	msv.SetReturnedValue([]byte{0, 0, 0, 0, 6, 118, 117, 101}, nil)          // "vue"
+	deliveryReportChan := make(chan types.EventReport, 10)
+	asyncProducer.SetAsyncMode(deliveryReportChan)
+	var sendMessageError error
+	for i := 0; i < 4; i++ {
+		sendMessageError = asyncProducer.SendSchemaBasedMessage(topicName, eventDataList[i].key, eventDataList[i].value, keySchemaId, valueSchemaId)
+		if sendMessageError != nil {
+			t.Error(sendMessageError)
+			return
+		}
+	}
+	if len(deliveryReportChan) > 0 {
+		t.Errorf("the report channel was already filled")
+		return
+	}
+	timeout, _ := time.ParseDuration("5s")
+	for i := 0; i < 4; i++ {
+		message, consumerError := consumer.ReadMessage(timeout)
+		if consumerError != nil {
+			t.Error(consumerError)
+			return
+		}
+		consumedKeySchemaId := binary.BigEndian.Uint32(message.Key[1:5])
+		consumedValueSchemaId := binary.BigEndian.Uint32(message.Value[1:5])
+		if int(consumedKeySchemaId) != keySchemaId || int(consumedValueSchemaId) != valueSchemaId {
+			t.Errorf("Error on write schema ids")
+			return
+		}
+		consumedKey := string(message.Key[5:])
+		consumedValue := string(message.Value[5:])
+		if consumedKey != eventDataList[i].key || consumedValue != eventDataList[i].value {
+			t.Errorf("Error on write data")
+			return
+		}
+	}
+	if len(deliveryReportChan) != 4 {
+		t.Errorf("invalid number of reports: %d", len(deliveryReportChan))
+		return
+	}
+	for i := 0; i < 4; i++ {
+		messageReport := <-deliveryReportChan
+		if messageReport.ErrorData != nil {
+			t.Error(messageReport.ErrorData)
+			return
+		}
+	}
+}
+
+func TestSendMessageAsyncModeEncodeSyncError(t *testing.T) {
+	defer msv.FlushEncodeReturnList()
+	key := "key"
+	value := "value"
+	keySchemaId := 5
+	valueSchemaId := 6
+	msv.SetReturnedValue(nil, errors.New("custom error 2 test on key"))
+	msv.SetReturnedValue(nil, errors.New("custom error 2 test on value"))
+	deliveryReportChan := make(chan types.EventReport, 10)
+	asyncProducer.SetAsyncMode(deliveryReportChan)
+	sendMessageError := asyncProducer.SendSchemaBasedMessage(topicName, key, value, keySchemaId, valueSchemaId)
+	if sendMessageError == nil {
+		t.Error("should return a sync error")
+		return
+	}
+	if len(deliveryReportChan) > 0 {
+		t.Errorf("the report channel was already filled")
+		return
+	}
+	timeout, _ := time.ParseDuration("5s")
+	_, consumerError := consumer.ReadMessage(timeout)
+	if consumerError == nil {
+		t.Error("consumer should timeout")
+		return
+	}
+	if !strings.Contains(consumerError.Error(), "Timed out") {
+		t.Errorf("unexpected error \"%s\"", consumerError.Error())
+		return
+	}
+	if len(deliveryReportChan) > 0 {
+		t.Errorf("invalid number of reports: %d", len(deliveryReportChan))
 		return
 	}
 }
@@ -109,10 +228,22 @@ func TestMain(m *testing.M) {
 			fmt.Println(log)
 		}
 	}()
-	producer = KafkaProducer{}
-	err = producer.Init(producerConfig, &msv, logChannel)
+	syncProducer = KafkaProducer{}
+	err = syncProducer.Init(producerConfig, &msv, logChannel)
 	if err != nil {
-		fmt.Printf("Error on producer init: %v", err)
+		fmt.Printf("Error on sync producer init: %v", err)
+		os.Exit(1)
+	}
+	asyncProducer = KafkaProducer{}
+	err = asyncProducer.Init(producerConfig, &msv, logChannel)
+	if err != nil {
+		fmt.Printf("Error on async producer init: %v", err)
+		os.Exit(1)
+	}
+	deliveryReportChan = make(chan types.EventReport, 10)
+	err = asyncProducer.SetAsyncMode(deliveryReportChan)
+	if err != nil {
+		fmt.Printf("Error on set producer async mode: %v", err)
 		os.Exit(1)
 	}
 	exitCode := m.Run()
